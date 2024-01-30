63c675a7a0d27cd56be7998387fffa754f05b231
Angela Amat y Leon <angela.amatyleon@kellogg.northwestern.edu>
2024-01-30 06:13:32 +0000

    finalized practice

--- practice.rb
+++ practice.rb
@@ -15,7 +15,6 @@
 # Evan's KelloggCoin balance is 10350
 # Anthony's KelloggCoin balance is 2650
 
-# 👇👇👇 Your code HERE 👇👇👇
 
 blockchain = [
   { "from_user" => nil, "to_user" => "ben", "amount" => 20000 },
@@ -28,17 +27,14 @@ blockchain = [
   { "from_user" => "anthony", "to_user" => "evan", "amount" => 1750 }
 ]
 
-# Create a new hash of the balances 
+# 👇👇👇 Your code HERE 👇👇👇
+
+# Create a new hash of the balances, if it doesn't have a value it sets it to 0
+# the hash created outside the loop will exist outside the loop and be updated 
 balances = Hash.new(0)
 
-puts balances
-
-# Calculate balances
-
-# blockchain.each will iterated over each element in the blockchain array (square brackets)
-# do |transaction| indicates that for each element in the blockhain array
-# , we are assigning the current element to the variables "transaction",
-# and then executing the code block that follows
+# array.each iterates over each element in the array
+# do |element| end defines a block of code that will be executed for each element in the array
 
 blockchain.each do |transaction|
   from_user = transaction["from_user"]
@@ -46,17 +42,20 @@ blockchain.each do |transaction|
   amount = transaction["amount"]
 
   # Subtract amount from sender's balance if applicable
-
-  # balances[from_user] accesses the current blance associated with from_user in the balances hash
-  # if there's not existing balance for from_user, Ruby initializes it to 0
-  # if from_user is not nil, if it is nil the line is skipped
-  balances[from_user] = balances[from_user] -  amount  if from_user
+  
+  # if from_user translates to if from_user is not nil
+  # if it is nil, it would just skip this code
+  if from_user
+     balances[from_user] = balances[from_user] -  amount 
+  end
 
   # Add amount to receiver's balance
 
-  #balances[to_user] accesses the current balance associated with the "to_user" in the balances hash
-  # if there's no existing balance, it's set to 0
+  # balances[to_user] pulls the value associated with transaction["to_user"] in the given element/key, e.g. ben
+  # if ben didn't exist as a key in balances, it would create a new key for ben and set the value to 0
+  # then it references the value of balances["ben"] = 0 + amount
   balances[to_user] = balances[to_user] + amount
+
 end
 
 puts balances 
